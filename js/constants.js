bcd3496461b6664ed5e9d954300abc815ef4ff57
Marco Frey <167756022+Marco-Melchio@users.noreply.github.com>
2025-11-20 13:58:35 +0100

    Use tcgdex.dev for TCG cards

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,6 @@
 const POKE_API_BASE = 'https://pokeapi.co/api/v2/pokemon/';
 const POKE_TYPE_ENDPOINT = 'https://pokeapi.co/api/v2/type/';
 const POKEMON_LIST_ENDPOINT = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
-const TCG_ENDPOINT = 'https://tcgdex.dev/de/rest/cards';
+const TCG_ENDPOINT = 'https://tcgdex.dev/v2/de/cards';
 const TEAM_STORAGE_KEY = 'pokemon-team';
 const TEAM_LIMIT = 6;
