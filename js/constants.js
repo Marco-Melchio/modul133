a58998a16191284a4618ab6f7e26382b18357a8f
Marco Frey <167756022+Marco-Melchio@users.noreply.github.com>
2025-11-21 17:01:53 +0100

    fixed TCG de2en
    added picture
    changed de2en

--- js/constants.js
+++ js/constants.js
@@ -1,11 +1,14 @@
 // Basis-URLs und Endpunkte der PokeAPI für einzelne Pokémon, Typen und die gesamte Namensliste.
 const POKE_API_BASE = 'https://pokeapi.co/api/v2/pokemon/';
+//ENDPOINT1
 const POKE_TYPE_ENDPOINT = 'https://pokeapi.co/api/v2/type/';
+//ENDPOINT2
 const POKEMON_LIST_ENDPOINT = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
 
 // TCGdex API-Konfiguration für das Laden der deutschsprachigen Karten.
 const TCG_API_BASE = 'https://api.tcgdex.net/v2';
-const TCG_LANGUAGE = 'de';
+const TCG_LANGUAGE = 'en';
+//ENDPOINT3
 const TCG_ENDPOINT = `${TCG_API_BASE}/${TCG_LANGUAGE}/cards`;
 
 // Lokale Storage-Konstanten für das Team-Management.
