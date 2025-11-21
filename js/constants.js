f6cbff3dc32561807f5c11ccefdfbeca5ae8c16f
Marco Frey <167756022+Marco-Melchio@users.noreply.github.com>
2025-11-21 11:32:47 +0100

    Add explanatory comments to HTML and CSS

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,13 @@
+// Basis-URLs und Endpunkte der PokeAPI für einzelne Pokémon, Typen und die gesamte Namensliste.
 const POKE_API_BASE = 'https://pokeapi.co/api/v2/pokemon/';
 const POKE_TYPE_ENDPOINT = 'https://pokeapi.co/api/v2/type/';
 const POKEMON_LIST_ENDPOINT = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
+
+// TCGdex API-Konfiguration für das Laden der deutschsprachigen Karten.
 const TCG_API_BASE = 'https://api.tcgdex.net/v2';
 const TCG_LANGUAGE = 'de';
 const TCG_ENDPOINT = `${TCG_API_BASE}/${TCG_LANGUAGE}/cards`;
+
+// Lokale Storage-Konstanten für das Team-Management.
 const TEAM_STORAGE_KEY = 'pokemon-team';
 const TEAM_LIMIT = 6;
